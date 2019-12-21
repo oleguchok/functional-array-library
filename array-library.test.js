fc14fe6e34831ae0ef15887acc53ec836b4de19e
Oleg Guchok <oleguchok@gmail.com>
2019-12-21 15:46:26 +0300

    Implemented skip method. Added unit tests for it

--- array-library.test.js
+++ array-library.test.js
@@ -71,3 +71,75 @@ test('should return new array with first n elements', function() {
     var array = [1,2,3];
     expect(lib.take(array, 2)).toEqual([1,2]);
 });
+
+/* skip tests */
+test('skip should throw TypeError if array parameter is null', function() {
+    var lib = new ArrayLibrary();
+    expect(function() {
+        return lib.skip(null);
+    }).toThrow(TypeError);
+});
+
+test('skip should throw TypeError if array parameter is undefined', function() {
+    var lib = new ArrayLibrary();
+    expect(function() {
+        return lib.skip(undefined);
+    }).toThrow(TypeError);
+});
+
+test('skip should throw TypeError if array parameter is not array', function() {
+    var lib = new ArrayLibrary();
+    expect(function() {
+        return lib.skip({});
+    }).toThrow(TypeError);
+
+    expect(function() {
+        return lib.skip('string');
+    }).toThrow(TypeError);
+
+    expect(function() {
+        return lib.skip(666);
+    }).toThrow(TypeError);
+});
+
+test('skip should return an array if n parameter is not provided', function() {
+    var lib = new ArrayLibrary();
+    var array = [1,2,3];
+    expect(lib.skip(array)).toEqual(array);
+});
+
+test('skip should return an array if n parameter is null', function() {
+    var lib = new ArrayLibrary();
+    var array = [1,2,3];
+    expect(lib.skip(array, null)).toEqual(array);
+});
+
+test('skip should return an array if n parameter is NaN', function() {
+    var lib = new ArrayLibrary();
+    var array = [1,2,3];
+    expect(lib.skip(array, NaN)).toEqual(array);
+});
+
+test('skip should return an array if n parameter is lower than 0', function() {
+    var lib = new ArrayLibrary();
+    var array = [1,2,3];
+    expect(lib.skip(array, -666)).toEqual(array);
+});
+
+test('skip should return an array if n parameter is 0', function() {
+    var lib = new ArrayLibrary();
+    var array = [1,2,3];
+    expect(lib.skip(array, 0)).toEqual(array);
+});
+
+test('skip should return an empty array if n parameter is greater than array length', function() {
+    var lib = new ArrayLibrary();
+    var array = [1,2,3];
+    expect(lib.skip(array, array.length + 10)).toEqual([]);
+});
+
+test('skip should return new array without skipped first n elements', function() {
+    var lib = new ArrayLibrary();
+    var array = [1,2,3];
+    expect(lib.skip(array, 2)).toEqual([3]);
+});
--- index.js
+++ index.js
@@ -20,19 +20,12 @@ ArrayLibrary.prototype.take = function(array, n) {
 
 ArrayLibrary.prototype.skip = function(array, n) {
     checkIsItAnArray(array);
-}
 
-// skip tests:
-// skip(null)
-// skip(undefined)
-// skip(not array)
-// skip(arr) : return arr
-// skip(arr, n), where n undefined : return arr
-// skip(arr, n), where n null : return arr
-// skip(arr, n), where n NaN : return arr
-// skip(arr, n), where n < 0 : return arr
-// skip(arr, n), where n == 0 : return arr
-// skip(arr, n), where n > arr.length : return []
-// skip(arr, n), where n < arr.length && n > 0: skip first n elements of arr and return left arr elements
+    if (n > 0) {
+        return array.slice(n);
+    }
+
+    return array;
+}
 
 module.exports = ArrayLibrary;
